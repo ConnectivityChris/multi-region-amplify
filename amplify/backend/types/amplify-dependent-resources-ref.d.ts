f40d9ef1b9d892333fb3d0ce0ee0daf3e620d5dc
Chris Howard <chris.howard@mechanicalrock.io>
2021-11-30 13:02:30 +0800

    back to basics

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,25 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "multiregionamplifyb1c07098": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "multiregionamplify": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "userstorage": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
